73c6a651c8238b0dcd7d9984455d0e18f98f7191
Maciej Ostrowski <maciej.ostrowski@fremtind.no>
2025-01-28 18:58:28 +0100

    Set up dependencies, TS aliases and initial folder structure.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./setupTests.ts",
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@components",
+        replacement: path.join(__dirname, "src/components"),
+      },
+      {
+        find: "@model",
+        replacement: path.join(__dirname, "src/model"),
+      },
+      {
+        find: "@common",
+        replacement: path.join(__dirname, "src/common"),
+      },
+      {
+        find: "src",
+        replacement: path.join(__dirname, "src"),
+      },
+    ],
+  },
 });
